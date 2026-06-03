65e13c295b4998139500f882b74fee23616ebbf1
Jay <Jorsinkaiye@gmail.com>
2026-06-03 16:25:51 -0400

    sigh

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,27 +1,13 @@
 export default function Hero() {
     return (
-        <section id = "home" className = "hero">
+        <div className="gradient-bg">
 
-            <div className="gradient-bg">
+            <div className="gradients-container">
 
-                <div className="gradients-container">
-
-                    <div className="g-1"></div>
-
-                </div>
+                <div className="g1"></div>
 
             </div>
 
-
-            {/*<div className= "hero-text">*/}
-            {/*    <h1 className = "hero-greeting">*/}
-            {/*        Hello, <br /> I&#39;m Jay*/}
-            {/*    </h1>*/}
-            {/*</div>*/}
-
-            {/*<div className = "hero-visual">*/}
-            {/*    <div className = "gradient-blob"></div>*/}
-            {/*</div>*/}
-        </section>
+        </div>
 );
 }
